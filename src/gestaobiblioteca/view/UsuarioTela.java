d6bd52751e18c4ec53e124bb8aa6c396805c4c46
hermanwiens <136393033+hermanwiens@users.noreply.github.com>
2026-03-16 22:46:34 +0000

    aula03

--- src/gestaobiblioteca/view/UsuarioTela.java
+++ src/gestaobiblioteca/view/UsuarioTela.java
@@ -6,8 +6,7 @@ public class UsuarioTela {
     public static void main(String[] args) {
             System.out.println("Tela de Usuário");
 
-            Usuario usuario = new Usuario() {
-            };
+            Usuario usuario = new Usuario(0, "João Silva", "joao.silva@example.com", "joao.silva", "senha123");
             usuario.setId(1);
             System.out.println("ID do usuário: " + usuario.getId());
         
